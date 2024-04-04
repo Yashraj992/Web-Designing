96c6e055b7878018356027ef047475725a1661bc
Yashraj992 <136678998+Yashraj992@users.noreply.github.com>
2024-04-04 16:51:25 +0530

    Delete Assigment 1/Module 6 directory

--- Assigment 1/Module 6/JavaScipt/Que18.js
+++
@@ -1,8 +0,0 @@
-function show(){
-    let show = document.getElementById('password');
-         if(checkbox.checked){
-            show.type = 'text'
-         }
-         else{
-             show.type = 'password'
-         }}
